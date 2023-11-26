dc3dd6af2d138f10c5c22ac50a1f3c623e7ab192
ssorin92 <149809120+ssorin92@users.noreply.github.com>
2023-11-26 15:44:12 +0000

    Documented all the java files for better understanding
    Documented all the java files for better understanding of whoever might be reading the project in the future, while also preparing the files for the creation of JavaDocs.

--- src/main/java/A03/ColorTable.java
+++ src/main/java/A03/ColorTable.java
@@ -2,11 +2,31 @@ package A03;
 import java.util.HashSet;
 import java.util.Set;
 
+
+/**
+ * The ColorTable class represents a palette of RGB colors. It stores unique color values and
+ * enforces constraints on the size and validity of the colors.
+ */
+
 public class ColorTable {
+
+    /**
+     * The size of the color palette, representing the maximum number of colors allowed.
+     */
     private final int paletteSize;
+
+    /**
+     * Set to store unique RGB color values in the color palette.
+     */
     private final Set<Integer> colorSet;
 
-
+    /**
+     * Constructs a ColorTable object with the specified palette size.
+     *
+     * @param paletteSize The number of colors in the palette. Must be a power of two,
+     * greater than 1, and less than or equal to 1024.
+     * @throws InvalidPaletteSizeException If an invalid palette size is provided.
+     */
     public ColorTable(int paletteSize) {
         if (!isValidPaletteSize(paletteSize)) {
             throw new InvalidPaletteSizeException();
@@ -16,7 +36,14 @@ public class ColorTable {
         this.colorSet = new HashSet<>();
     }
 
-
+    /**
+     * Adds a 24-bit RGB color to the color palette.
+     *
+     * @param rgbValue The RGB color value to add.
+     * @throws InvalidRGBValueException If an invalid RGB color value is provided.
+     * @throws DuplicateRGBValueException If the RGB color is already present in the palette.
+     * @throws ExceedingCapacityException If the color palette is already at its maximum capacity.
+     */
     public void add(int rgbValue) {
         if (!isValidRgbValue(rgbValue)) {
             throw new InvalidRGBValueException();
@@ -30,19 +57,43 @@ public class ColorTable {
         colorSet.add(rgbValue);
     }
 
-
+    /**
+     * Retrieves the size of the color palette.
+     *
+     * @return The size of the color palette.
+     */
     public int getPaletteSize() {
         return paletteSize;
     }
+
+    /**
+     * Validates if the provided size is a valid palette size.
+     *
+     * @param size The size to validate.
+     * @return True if the size is a power of two, greater than 1, and less than or equal to 1024; otherwise, false.
+     */
     private boolean isValidPaletteSize(int size) {
         return size > 1 && size <= 1024 && (size & (size - 1)) == 0;
     }
 
+    /**
+     * Checks if the color palette contains a specific RGB color value.
+     *
+     * @param rgbValue The RGB color value to check.
+     * @return True if the color palette contains the specified RGB color; otherwise, false.
+     */
     public boolean contains(int rgbValue) {
 
         return colorSet.contains(rgbValue);
     }
 
+
+    /**
+     * Validates if the provided RGB color value is within the valid range.
+     *
+     * @param rgbValue The RGB color value to validate.
+     * @return True if the RGB value is a 24-bit value; otherwise, false.
+     */
     private boolean isValidRgbValue(int rgbValue) {
         // Assuming a valid RGB value is a 24-bit value
         return (rgbValue >= 0 && rgbValue <= 0xFFFFFF);
--- src/main/java/A03/DuplicateRGBValueException.java
+++ src/main/java/A03/DuplicateRGBValueException.java
@@ -1,5 +1,9 @@
 package A03;
 
+/**
+ * Exception handling for when the EGB value about to be introduced is
+ * already in the palette, and we dont want a duplicate of it.
+ */
 public class DuplicateRGBValueException extends RuntimeException {
     public DuplicateRGBValueException() {
 

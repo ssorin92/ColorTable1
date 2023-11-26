093c54696b058fea440b1acbcb4719281ccad372
ssorin92 <149809120+ssorin92@users.noreply.github.com>
2023-11-26 14:48:59 +0000

    Test code : Constructor for a ColorTable object with a valid palette size
    Written the test that checks the constructor works properly with a VALID value for the palette size

--- src/test/java/A03/ColorTableTest.java
+++ src/test/java/A03/ColorTableTest.java
@@ -1,4 +1,18 @@
 package A03;
+import org.junit.jupiter.api.Test;
+import java.awt.*;
 
 public class ColorTableTest {
+    /**
+     * First test is the one checking if the constructor functions properly with a
+     * valid value for the palette size (ie 4 in this case)
+     * We create a new instance of a colortable, and test.
+     */
+    @Test
+    public void testConstructorWithValidPaletteSize(){
+        int paletteSize=4;
+
+        ColorTable colorTable= new ColorTable(paletteSize);
+        assertEquals(paletteSize,colorTable.getPaletteSize());
+    }
 }
